f27bac25fc09db118d2ae53ef28877e53d75e35a
Omar Ajao <96845313+SnolTwig@users.noreply.github.com>
2025-11-04 23:56:01 +0000

    finished Area finding project and and created file for second project

--- elite101/hw.py
+++ elite101/hw.py
@@ -4,9 +4,10 @@ def count_vowels(word):
     count = 0
     for i in word:
         for j in vowel:
+            breakpoint()
             if i == j:
-                count = count +1
+                count += 1
     return count
-
 word = input("Enter String: ")
 print(count_vowels(word))
+breakpoint()
